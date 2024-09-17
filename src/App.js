1f8dc28dda26d59c49cb480762f4d2d47fd8a7e9
Alok3333 <alokprajapati697@gmail.com>
2024-09-17 19:55:33 +0530

    setup and created some folder with files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StefansLawVerification from "./pages/StefansLaw";
+
+function App() {
+  return (
+    <div className="App">
+      <StefansLawVerification/>
+    </div>
+  );
+}
+
+export default App;
