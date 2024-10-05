a59f0c4422d2fb0d217897de2055191bfd1662b6
Alok3333 <alokprajapati697@gmail.com>
2024-10-05 19:52:59 +0530

    turtle and rabbit game with animation and level wise

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+import BitSerial from "./pages/BitSerialGame";
+import CatPuzzle from "./pages/CatPuzzle";
+import DigitalTriradii from "./pages/DigitalTriradiiGame";
+import DragDrop from "./pages/DragDrop";
+import FingerPrintPatterns from "./pages/FingerPrintPatternsGame";
+import GetMold from "./pages/GetMoldGame";
+import Iot1 from "./pages/Iot1";
+import MassSpectrocopy from "./pages/MassSpectrocopy";
+import MendelsLaw from "./pages/MendelsLaw";
+import NuclearMagneticResosnance from "./pages/NuclearMagneticResosnance";
+import OpticalFibre from "./pages/OpticalFibreGame";
+import PhaseSequenceSynchronous from "./pages/PhaseSequenceSynchronous";
+import QuickTap from "./pages/QuickTap";
+import RaceGame from "./pages/Racegame";
+import Simulation from "./pages/Simulation";
+import SkeletonExp from "./pages/SkeletonExpGame";
+import SkeletonExpPart2 from "./pages/SkeletonExpPart2Game";
+import StefansLawVerification from "./pages/StefansLawGame";
+import TurtleRabbit from "./pages/TurtleGame";
+import VideoPage31 from "./pages/Videopage31";
+import VideoPage from "./pages/Videopage32a";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <VideoPage31 /> */}
+      {/* <VideoPage/> */}
+      {/* <DragDrop/> */}
+      {/* <Simulation /> */}
+      {/* <PhaseSequenceSynchronous/> */}
+
+      {/* <MendelsLaw /> */}
+
+      {/* <GetMold/> */}
+      {/* <DigitalTriradii/> */}
+      {/* <SkeletonExpPart2 /> */}
+      {/* <OpticalFibre/> */}
+      {/* <StefansLawVerification/> */}
+      {/* <FingerPrintPatterns /> */}
+      {/* <SkeletonExp/> */}
+      {/* <BitSerial/> */}
+
+      {/* <NuclearMagneticResosnance /> */}
+      {/* <MassSpectrocopy/> pendding*/}
+
+      {/* <Iot1/> */}
+      {/* <CatPuzzle/> */}
+      {/* <TurtleRabbit/> */}
+      <RaceGame/>
+      {/* <QuickTap/> */}
+    </div>
+  );
+}
+
+export default App;
